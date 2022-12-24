b26e25cb25d9b7ea4ff561c1443332dbc6f91b7b
Julie Winchester <julie.winchester@duke.edu>
2022-12-23 19:55:15 -0600

    Update libWrapper classes

--- module/module.js
+++ module/module.js
@@ -3,11 +3,11 @@ import { rollAbilitySaveWrapper, rollAbilityTestWrapper, rollSkillWrapper } from
 import { configureDialog, _onPartToggle } from "./d20-roll.js";
 
 Hooks.once('init', async function() {
-  libWrapper.register("modify-rolls", "game.dnd5e.documents.Actor5e.prototype.rollAbilitySave", rollAbilitySaveWrapper, "WRAPPER");
-  libWrapper.register("modify-rolls", "game.dnd5e.documents.Actor5e.prototype.rollAbilityTest", rollAbilityTestWrapper, "WRAPPER");
-  libWrapper.register("modify-rolls", "game.dnd5e.documents.Actor5e.prototype.rollSkill", rollSkillWrapper, "WRAPPER");
-  libWrapper.register("modify-rolls", "game.dnd5e.documents.Item5e.prototype.rollAttack", rollAttackWrapper, "WRAPPER");
-  libWrapper.register("modify-rolls", "game.dnd5e.documents.Item5e.prototype.rollToolCheck", rollToolCheckWrapper, "WRAPPER");
+  libWrapper.register("modify-rolls", "CONFIG.Actor.documentClass.prototype.rollAbilitySave", rollAbilitySaveWrapper, "WRAPPER");
+  libWrapper.register("modify-rolls", "CONFIG.Actor.documentClass.prototype.rollAbilityTest", rollAbilityTestWrapper, "WRAPPER");
+  libWrapper.register("modify-rolls", "CONFIG.Actor.documentClass.prototype.rollSkill", rollSkillWrapper, "WRAPPER");
+  libWrapper.register("modify-rolls", "CONFIG.Item.documentClass.prototype.rollAttack", rollAttackWrapper, "WRAPPER");
+  libWrapper.register("modify-rolls", "CONFIG.Item.documentClass.prototype.rollToolCheck", rollToolCheckWrapper, "WRAPPER");
   libWrapper.register("modify-rolls", "game.dnd5e.dice.D20Roll.prototype.configureDialog", configureDialog, "OVERRIDE");
   game.dnd5e.dice.D20Roll.prototype._onPartToggle = _onPartToggle;
 });
