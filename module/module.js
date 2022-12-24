b177cb04bfe4d971e2c3200275cb68b022513fac
Julie Winchester <julie.winchester@duke.edu>
2022-12-23 19:32:42 -0600

    Rename modifyRolls.js to module.js

--- module/module.js
+++ module/module.js
@@ -1,5 +1,6 @@
 import { rollAttackWrapper, rollToolCheckWrapper } from "./item.js";
 import { rollAbilitySaveWrapper, rollAbilityTestWrapper, rollSkillWrapper } from "./actor.js";
+import { configureDialog, _onPartToggle } from "./d20-roll.js";
 
 Hooks.once('init', async function() {
   libWrapper.register("modify-rolls", "game.dnd5e.documents.Actor5e.prototype.rollAbilitySave", rollAbilitySaveWrapper, "WRAPPER");
@@ -7,4 +8,6 @@ Hooks.once('init', async function() {
   libWrapper.register("modify-rolls", "game.dnd5e.documents.Actor5e.prototype.rollSkill", rollSkillWrapper, "WRAPPER");
   libWrapper.register("modify-rolls", "game.dnd5e.documents.Item5e.prototype.rollAttack", rollAttackWrapper, "WRAPPER");
   libWrapper.register("modify-rolls", "game.dnd5e.documents.Item5e.prototype.rollToolCheck", rollToolCheckWrapper, "WRAPPER");
+  libWrapper.register("modify-rolls", "game.dnd5e.dice.D20Roll.prototype.configureDialog", configureDialog, "OVERRIDE");
+  game.dnd5e.dice.D20Roll.prototype._onPartToggle = _onPartToggle;
 });
