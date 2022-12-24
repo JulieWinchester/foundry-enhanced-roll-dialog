510f0c041de022345c9ce1ef3cd13c47373ba1fc
Julie Winchester <julie.winchester@duke.edu>
2022-12-23 18:30:48 -0600

    Refactor getAttackToHitWrapper to rollAttackWrapper

--- module/item.js
+++ module/item.js
@@ -1,61 +1,49 @@
 import RollEffectChanges from "./roll-effect-changes.js";
 
-export function getAttackToHitWrapper(wrapped, ...args) {
-  let results = wrapped(...args);
+export function rollAttackWrapper(wrapped, options={}) {
+  if ( !this.hasAttack ) throw new Error("You may not place an Attack Roll with this Item.");
+  
+  // Get the parts and rollData for this item's attack
+  let {parts, rollData} = this.getAttackToHit();
 
-  if (results) {
-    let {rollData, parts} = results;
+  // Additional roll data terms
+  let addlRollData = {};
 
-    // If item bonus present, reparse as @attr
-    if (parseInt(rollData?.item?.attackBonus) && (parts || []).includes(rollData?.item?.attackBonus)) {
-      rollData, parts = itemAttackBonusAsAttribute(rollData, parts);
-    }
-
-    // If actor bonus present, reparse as @attr
-    const actorAttackBonus = this.actor?.system?.bonuses?.[this.system.actionType]?.attack;
-    if ( (actorAttackBonus) && ( (parts || []).includes(actorAttackBonus) ) ) {
-      rollData, parts = actorAttackBonusAsAttribute(rollData, parts, actorAttackBonus);
-    }
-
-    const attributeOrder = ["@itemAttackBonus", "@mod", "@prof", "@actorAttackBonus", "@ammo"]
-
-    // Get ammo name if necessary
-    let ammoItemName = "";
-    if ( (this.system.consume?.type === "ammo") && this.actor?.items ) {
-      const ammoItem = this.actor.items.get(this.system.consume.target);
-      ammoItemName = ammoItem.name;
-    }
-
-    // Construct initial action info object
-    let action = {
-      attributeOrder: attributeOrder,
-      mode: "attack",
-      parts: parts,
-      changes: RollEffectChanges.getChanges(this.actor, parts, "attack", this.system.actionType),
-      itemName: this.name || "",
-      itemAbilityMod: this.abilityMod,
-      proficient: this.system.proficient,
-      ammoItemName: ammoItemName,
-    };
-    if (rollData) {
-      rollData.action = action;
-    } 
+  // If item bonus present, reparse as @attr
+  if (parseInt(rollData?.item?.attackBonus) && (parts || []).includes(rollData?.item?.attackBonus)) {
+    parts.splice(parts.indexOf(rollData.item.attackBonus), 1, "@itemAttackBonus");
+    addlRollData.itemAttackBonus = rollData.item.attackBonus;
   }
 
-  return results;
-}
+  // If actor bonus present, reparse as @attr
+  const actorAttackBonus = this.actor?.system?.bonuses?.[this.system.actionType]?.attack;
+  if ( (actorAttackBonus) && ( (parts || []).includes(actorAttackBonus) ) ) {
+    parts.splice(parts.lastIndexOf(actorAttackBonus), 1, "@actorAttackBonus")
+    addlRollData.actorAttackBonus = actorAttackBonus;
+  }
 
-export function itemAttackBonusAsAttribute(rollData, parts) {
-  parts.splice(parts.indexOf(rollData.item.attackBonus), 1, "@itemAttackBonus")
-  rollData.itemAttackBonus = rollData.item.attackBonus;
-  return rollData, parts;
-}
+  // Get ammo name if necessary
+  let ammoItemName = "";
+  if ( (this.system.consume?.type === "ammo") && this.actor?.items ) {
+    const ammoItem = this.actor.items.get(this.system.consume.target);
+    ammoItemName = ammoItem.name;
+  }
 
-export function actorAttackBonusAsAttribute(rollData, parts, actorAttackBonus) {
-  parts.splice(parts.lastIndexOf(actorAttackBonus), 1, "@actorAttackBonus")
-  rollData.actorAttackBonus = actorAttackBonus;
-  return rollData, parts;
-}
+  addlRollData.action = {
+    attributeOrder: ["@itemAttackBonus", "@mod", "@prof", "@actorAttackBonus", "@ammo"],
+    mode: "attack",
+    parts: parts,
+    changes: RollEffectChanges.getChanges(this.actor, parts, "attack", this.system.actionType),
+    itemName: this.name || "",
+    itemAbilityMod: this.abilityMod,
+    proficient: this.system.proficient,
+    ammoItemName: ammoItemName
+  };
+
+  options = foundry.utils.mergeObject(options, { parts, data: addlRollData });
+
+  return wrapped(options);
+} 
 
 export function rollToolCheckWrapper(wrapped, options) {
   const abl = this.system.ability;
--- module/modifyRolls.js
+++ module/modifyRolls.js
@@ -1,10 +1,10 @@
-import { getAttackToHitWrapper, rollToolCheckWrapper } from "./item.js";
+import { rollAttackWrapper, rollToolCheckWrapper } from "./item.js";
 import { rollAbilitySaveWrapper, rollAbilityTestWrapper, rollSkillWrapper } from "./actor.js";
 
 Hooks.once('init', async function() {
   libWrapper.register("modify-rolls", "game.dnd5e.documents.Actor5e.prototype.rollAbilitySave", rollAbilitySaveWrapper, "WRAPPER");
   libWrapper.register("modify-rolls", "game.dnd5e.documents.Actor5e.prototype.rollAbilityTest", rollAbilityTestWrapper, "WRAPPER");
   libWrapper.register("modify-rolls", "game.dnd5e.documents.Actor5e.prototype.rollSkill", rollSkillWrapper, "WRAPPER");
-  libWrapper.register("modify-rolls", "game.dnd5e.documents.Item5e.prototype.getAttackToHit", getAttackToHitWrapper, "WRAPPER");
+  libWrapper.register("modify-rolls", "game.dnd5e.documents.Item5e.prototype.rollAttack", rollAttackWrapper, "WRAPPER");
   libWrapper.register("modify-rolls", "game.dnd5e.documents.Item5e.prototype.rollToolCheck", rollToolCheckWrapper, "WRAPPER");
 });
